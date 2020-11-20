2d168ff71f678ac4ba457d322a07c57a5b8d586d
Benny Christensen <benny@potatismoose.com>
2020-11-20 13:53:59 +0100

    Addition som första operator fungerar som det ska

--- Inlämningsuppgift1/Program.cs
+++ Inlämningsuppgift1/Program.cs
@@ -10,18 +10,23 @@ namespace Inlämningsuppgift1
 
             List<char> inputOperator = new List<char>();
             List<int> inputNumber = new List<int>();
+            List<double> sum = new List<double>();
             bool quit = false;
 
+            Console.Title = "Inlämmningsuppgift 1";
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("  Inlämmningsuppgift 1");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine();
             do
             {
-                Console.Title = "Inlämmningsuppgift 1";
-                Console.WriteLine("-------------------------");
-                Console.WriteLine("  Inlämmningsuppgift 1");
-                Console.WriteLine("-------------------------");
-                Console.WriteLine();
+
                 //Nollställer listan med operatorer så att man kan knappa in en ny beräkning
-                if (inputOperator.Count > 0)
+                if (inputOperator.Count > 0 || inputNumber.Count > 0)
+                {
                     inputOperator.Clear();
+                    inputNumber.Clear();
+                }
                 //ber användaren ange första operatorn och sparar den till listan
                 Console.Write("Ange första operatorn (+,-,* eller /): ");
                 inputOperator.Add(Convert.ToChar(Console.ReadLine()));
@@ -35,25 +40,68 @@ namespace Inlämningsuppgift1
                 Console.Write("Ange det tredje talet: ");
                 inputNumber.Add(Convert.ToInt32(Console.ReadLine()));
                 Console.WriteLine();
-                for (int i = 0; i < inputOperator.Count; i++)
+                
+                bool error = default(bool);
+                if (inputOperator[0] == '+')
                 {
-                    Console.WriteLine("operator " + (i+1) + " = " + inputOperator[i]);
-
+                    if (inputOperator[1] == '+')
+                        sum.Add(inputNumber[0] + inputNumber[1] + inputNumber[2]);
+                    if (inputOperator[1] == '-')
+                        sum.Add(inputNumber[0] + inputNumber[1] - inputNumber[2]);
+                    if (inputOperator[1] == '*')
+                        sum.Add(inputNumber[0] + inputNumber[1] * inputNumber[2]);
+                    if (inputOperator[1] == '/')
+                        if (inputNumber[2] == 0)
+                        {
+                            Console.WriteLine("Du kan inte dela med 0");
+                            error = true;
+                        }
+                        else
+                            sum.Add(inputNumber[0] + inputNumber[1] / inputNumber[2]);
                 }
-
-                for (int i = 0; i < inputNumber.Count; i++)
+                
+                if (!error)
                 {
-                    Console.WriteLine("Tal " + (i + 1) + " = " + inputNumber[i]);
-
-                }
-
-
-
+                    int counter = sum.Count - 1;
+                    Console.Write("{0}{1}{2}{3}{4} = {5}", inputNumber[0], inputOperator[0], inputNumber[1], inputOperator[1], inputNumber[2], sum[counter]);
                 Console.WriteLine();
                 Console.ReadKey();
+                }
+                
+                error = false;
+                
+                do
+                {
+                    Console.Write("Vill du göra en ny uträkning? j/n:");
+                    char quitChoice = Convert.ToChar(Console.ReadLine());
+                    switch (quitChoice)
+                    {
+                        case 'j':
+                            quit = default(bool);
+                            error = false;
+                            break;
+                        case 'n':
+                            quit = true;
+                            error = false;
+                            break;
+                        default:
+                            error = true;
+                            break;
+                    }
+                } while (error);
+                error = false;
+                Console.Clear();
             } while (!quit);
+            
 
-
+            double summa = default(double);
+            for (int i = 0; i < sum.Count; i++)
+            {
+                summa += sum[i];
+            }
+            
+            Console.WriteLine("\nDen totala summan av alla uträkningar blev: {0}", summa);
+            Console.ReadKey();
 
         }
     }
